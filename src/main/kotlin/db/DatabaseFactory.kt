8f55de312778e1e50c627bb172eb1cf09dedb0d5
Rafael <rafael.s.costa96@gmail.com>
2019-11-03 20:08:36 +0000

    Database factory start

---
+++ src/main/kotlin/db/DatabaseFactory.kt
@@ -0,0 +1,23 @@
+package db
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.sql.Database
+
+class DatabaseFactory {
+
+    fun init() {
+        Database.connect(hikari())
+    }
+
+    private fun hikari(): HikariDataSource {
+        val config = HikariConfig()
+        config.driverClassName = "org.h2.Driver"
+        config.jdbcUrl = "jdbc:h2:mem:test"
+        config.maximumPoolSize = 3
+        config.isAutoCommit = false
+        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        config.validate()
+        return HikariDataSource(config)
+    }
+}
