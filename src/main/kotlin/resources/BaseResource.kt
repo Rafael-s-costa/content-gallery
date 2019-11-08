fa81264c7fa6b8c7fc01336f5c06f849cdff544f
Rafael <rafael.s.costa96@gmail.com>
2019-11-08 00:24:26 +0000

    Added logger

--- src/main/kotlin/resources/BaseResource.kt
+++ src/main/kotlin/resources/BaseResource.kt
@@ -1,6 +1,7 @@
 package resources
 
 import api.BaseResponse
+import common.getLogger
 import io.ktor.application.call
 import io.ktor.response.respond
 import io.ktor.routing.Route
@@ -9,6 +10,7 @@ import io.ktor.routing.get
 fun Route.home() {
     get("/") {
         val response = BaseResponse(1000, "SUCCESS", "Hello");
+        getLogger().info("Received request for / ")
         call.respond(response)
     }
 }
